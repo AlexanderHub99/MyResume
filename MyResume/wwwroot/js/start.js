42fbd9da914bf71e1310ab18f80920575a81d2f5
DESKTOP-LUIH41M\Sasha1648 <agames1448@gmail.com>
2021-12-31 14:20:26 +0300

    Img блок с фото

---
+++ MyResume/wwwroot/js/start.js
@@ -0,0 +1,8 @@
+﻿jQuery(document).ready(function () {
+    
+    particlesJS.load('particlesJS', '/particles.json',
+    function () {
+        console.log('callback - particles.js config loaded');
+    });
+
+});
