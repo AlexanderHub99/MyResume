0f3c4d7f46ed611039f8f8f115d5ba8e74a92071
DESKTOP-LUIH41M\Sasha1648 <agames1448@gmail.com>
2022-01-01 17:04:18 +0300

    пр

--- MyResume/wwwroot/js/start.js
+++
@@ -1,8 +0,0 @@
-﻿jQuery(document).ready(function () {
-    
-    particlesJS.load('particlesJS', '/particles.json',
-    function () {
-        console.log('callback - particles.js config loaded');
-    });
-
-});
