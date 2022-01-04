c39e6d9ef3c1624f2a86beb7f626fb097ebe2126
DESKTOP-LUIH41M\Sasha1648 <agames1448@gmail.com>
2022-01-04 18:11:27 +0300

    App API ToDoList

---
+++ MyResume/Controllers/APIToDoListsController.cs
@@ -0,0 +1,112 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyResume.Data;
+using MyResume.Models;
+
+namespace MyResume.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIToDoListsController : ControllerBase
+    {
+        private readonly WebToDoListContext _context;
+
+        public APIToDoListsController(WebToDoListContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/APIToDoLists
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoList>>> GetToDoList()
+        {
+            return await _context.ToDoList.ToListAsync();
+        }
+
+        // GET: api/APIToDoLists/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoList>> GetToDoList(int id)
+        {
+            var toDoList = await _context.ToDoList.FindAsync(id);
+
+            if (toDoList == null)
+            {
+                return NotFound();
+            }
+
+            return toDoList;
+        }
+
+        // PUT: api/APIToDoLists/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoList(int id, ToDoList toDoList)
+        {
+            if (id != toDoList.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDoList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/APIToDoLists
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ToDoList>> PostToDoList(ToDoList toDoList)
+        {
+            _context.ToDoList.Add(toDoList);
+            await _context.SaveChangesAsync();
+
+           // return CreatedAtAction("GetToDoList", new { id = toDoList.id }, toDoList);
+            return CreatedAtAction(nameof(GetToDoList), new { id = toDoList.id }, toDoList);
+        }
+       
+
+
+        // DELETE: api/APIToDoLists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDoList(int id)
+        {
+            var toDoList = await _context.ToDoList.FindAsync(id);
+            if (toDoList == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoList.Remove(toDoList);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ToDoListExists(int id)
+        {
+            return _context.ToDoList.Any(e => e.id == id);
+        }
+    }
+}
